29af85e8997bf1ca56eaf38183fb373ab3c5e89f
scorpio686 <31187145+scorpio686@users.noreply.github.com>
2018-08-26 20:17:32 +0200

    add french translation

---
+++ locales/fr.lua
@@ -0,0 +1,12 @@
+Locales ['fr'] = {
+	['input'] = 'Appuyez sur ~INPUT_PICKUP~ pour proposer de la drogue',
+	['reject'] = 'La personne a ~r~rejeté~s~ votre offre',
+	['no_more_drugs'] = 'Vous ~r~n'~w~avez ~r~plus ~w~de ~g~drogues!',
+	['too_far_away'] = 'Vous êtes trop loin! Vente ~r~annulée',
+	['remained'] = 'Restant~b~ ',
+	['you_have_sold'] = 'Vous avez vendu ',
+	['weed_pooch'] = ' pochon de ~g~weed~s~ pour~r~ ',
+	['meth_pooch'] = ' pochon de ~g~meth~s~ pour~r~ ',
+	['coke_pooch'] = ' pochon de ~g~cocaïne~s~ pour~r~ ',
+	['opium_pooch'] = ' pochon d\'~g~opium~s~ pour~r~ ',
+}
