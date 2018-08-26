87fc1d47b183d188bd1e9f25ed403eef0b79ddec
Viktor <35453587+LifeGoal@users.noreply.github.com>
2018-08-26 17:25:14 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -3,6 +3,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 server_scripts {
 	'locales/pl.lua',
 	'locales/en.lua',
+	'locales/sv.lua',
 	'server/main.lua',
 	'config.lua'
 }
@@ -11,6 +12,7 @@ client_scripts {
 	'@es_extended/locale.lua',
 	'locales/pl.lua',
 	'locales/en.lua',
+	'locales/sv.lua',
 	'client/main.lua',
 	'config.lua'
 }
