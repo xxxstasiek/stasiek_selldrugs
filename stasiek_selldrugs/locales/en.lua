6dcaac623c8726998e9a36bc4dc2f4ed7dfdd701
StasiekESX <42705773+StasiekESX@users.noreply.github.com>
2018-08-26 11:04:49 +0200

    Add files via upload

---
+++ stasiek_selldrugs/locales/en.lua
@@ -0,0 +1,10 @@
+Locales ['en'] = {
+	['input'] = 'Press ~INPUT_PICKUP~ to offer drugs',
+	['reject'] = 'Person ~r~reject~s~ your offer',
+	['no_more_drugs'] = '~r~You dont have~s~ more ~g~drugs!',
+	['too_far_away'] = 'You are too far away! Selling ~r~canceled',
+	['remained'] = 'Remained~b~ ',
+	['you_have_sold'] = 'You have sold ',
+	['weed_pooch'] = ' pooch of ~g~weed~s~ for~r~ ',
+	['meth_pooch'] = ' pooch of ~g~meth~s~ for~r~ ',
+}
