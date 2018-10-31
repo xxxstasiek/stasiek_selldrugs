e816483c696ba71838731742bb8f2e4bf27adc0b
Stasiek <42705773+StasiekESX@users.noreply.github.com>
2018-10-31 18:32:39 +0100

    update

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config                            = {}
 Config.Locale = 'pl' -- your language, It would be nice if you send me your translation on fivem forum
 Config.TimeToSell = 15 -- how many seconds player have to wait/stand near ped
-Config.CallCops = true -- if true and if ped reject your offer then there is 40% chance that ped will call cops
+Config.CallCops = true -- if true and if ped reject your offer then there is [Config.CallCopsPercent]% to call cops
 Config.CopsRequiredToSell = 2 -- required cops on server to sell drugs
 Config.CallCopsPercent = 1 -- (min1) if 1 cops will be called every time=100%, 2=50%, 3=33%, 4=25%, 5=20%
 Config.PedRejectPercent = 3 -- (min1) if 1 ped reject offer=100%, 2=50%, 3=33%, 4=25%, 5=20%
